1963ee4f84d79e6cfbf9fab901dbbc89e62e9435
Mario Lazzari <mario.lazzari@gmail.com>
2026-01-09 22:48:26 +0100

    Done lesson 6

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Support Me</h1>
+      <Button>Log In</Button>
+      <Button>Sign Up</Button>
+    </div>
+  );
+}
+
+export default LandingPage;
