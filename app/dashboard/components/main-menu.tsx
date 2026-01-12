abd9b4ecfc466f4bb77b87eb8933b20c3c067440
Mario Lazzari <mario.lazzari@gmail.com>
2026-01-12 08:25:50 +0100

    Done lesson 25

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,12 +1,20 @@
 import { Separator } from "@radix-ui/react-select";
 import { MenuTitle } from "./menu-title";
+import { MenuItem } from "./menu-item";
 
 export function MainMenu() {
   return (
     <div className="overflow-auto p-4">
       <MenuTitle />
       <Separator className="my-4 dark:bg-black bg-zinc-300" />
-      Main Menu
+
+      <div className="py-4">
+        <MenuItem href="/dashboard">My Dashboard</MenuItem>
+        <MenuItem href="/dashboard/teams">Teams</MenuItem>
+        <MenuItem href="/dashboard/employees">Employees</MenuItem>
+        <MenuItem href="/dashboard/account">Account</MenuItem>
+        <MenuItem href="/dashboard/settings">Settings</MenuItem>
+      </div>
     </div>
   );
 }
